8f51595a4cac909cd51fc79ce0c025c955757f88
Ayah Al-Hasan <ayahal-hasan@Ayahs-MacBook-Air.local>
2016-09-07 15:45:19 -0700

    some change in css

--- js/main.js
+++ js/main.js
@@ -1,47 +1,40 @@
-$(function (){
+$(document).ready(function(){
+  $(function (){
 
-  //flickity slider
+    //flickity slider
+    $('.main-carousel').flickity({
+    // options
+      cellAlign: 'right',
+      contain: true,
+      wrapAround: true,
+      prevNextButtons: false,
+      autoPlay: true,
+      imagesloaded: true
+    });
 
-  $('.main-carousel').flickity({
-  // options
-    cellAlign: 'right',
-    contain: true,
-    wrapAround: true,
-    prevNextButtons: false,
-    autoPlay: true,
-    imagesloaded: true
+    // A function to check for an email address
+    $('form.sign-up-form').on('submit', function(event){
+      event.preventDefault();
+      var userEmail = $('input[type="email"]').val();
+
+     if (userEmail) {
+        alert('Thank you for subscribing!');
+      } else {
+        alert('Please provide a valid email address!');
+      }
+    });
+
+    // smooth scrolling function
+    $('a[href^="#"]').on('click',function (e) {
+	     e.preventDefault();
+
+	     var $target = $(this.hash);
+
+	     $('html, body').stop().animate(
+          {
+	         'scrollTop': $target.offset() .top - ($('header').height())
+          },
+          900, 'swing');
+	     });
   });
-
-  // A function to check for an email address in the format
-
-  $('form.sign-up-form').on('submit', function(event){
-    event.preventDefault();
-     var userEmail = $('input[type="email"]').val();
-
-    if (userEmail) {
-      alert('Thank you for subscribing!');
-    } else {
-      alert('Please provide a valid email address!');
-    }
-  });
-
-  // smooth scrolling function, taking into account the fixed header
-
-  //$target.offset().top - height of header
-
-  $('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var $target = $(this.hash);
-
-	    $('html, body').stop().animate(
-        {
-	        'scrollTop': $target.offset().top - ($('header').height())
-        },
-        900, 'swing');
-	});
-
-
-
-
 });
